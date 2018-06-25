5eade43f4f23fbc9dab60c6c0a7d34c44e5f7b92
Amish Shah <amishshah.2k@gmail.com>
2018-06-25 10:31:47 +0100

    more

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
-const Game = new PIXI.Application();
+const game = new PIXI.Application();
 
 const ASSETS = {
     spaceship: 'http://gipsypixel.com/wp-content/uploads/2017/11/Desktop-For-Cute-Cat-Wallpapercom-Animals-Images-Hd-Pics-Smartphone.jpg',
 };
+
+document.body.appendChild(game.view);
+
+const sprites = {};
 
 function loadAssets() {
     const loader = PIXI.loader;
@@ -12,7 +16,23 @@ function loadAssets() {
     }
 
     loader.load((loader, resources) => {
-        alert(resources);
+        spaceship = sprites.spaceship = new PIXI.Sprite(resources.spaceship.texture);
+        // Setup the position of the spaceship
+    spaceship.x = game.renderer.width / 2;
+    spaceship.y = game.renderer.height / 2;
+
+    // Rotate around the center
+    spaceship.anchor.x = 0.5;
+    spaceship.anchor.y = 0.5;
+
+    // Add the spaceship to the scene we are building
+    game.stage.addChild(spaceship);
+
+    // Listen for frame updates
+    game.ticker.add(() => {
+         // each frame we spin the spaceship around a bit
+        spaceship.rotation += 0.01;
+    });
     });
 }
 
